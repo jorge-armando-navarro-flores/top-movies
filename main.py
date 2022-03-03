101cb7566c30f572247516a97e0722d71534308f
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-03-03 12:23:12 -0600

    find_movie

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+import os
+
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -19,9 +24,9 @@ class Movie(db.Model):
     title = db.Column(db.String(250), unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(500), unique=True, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    ranking = db.Column(db.Integer, nullable=False)
-    review = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float)
+    ranking = db.Column(db.Integer)
+    review = db.Column(db.String(250))
     img_url = db.Column(db.String(250), nullable=False)
 
     def __repr__(self):
@@ -91,8 +96,30 @@ def add():
     add_form = AddForm()
     if add_form.validate_on_submit():
         movie_title = add_form.title.data
-        requests.get()
+        params = {
+            "api_key": os.environ.get("MOVIES_API_KEY"),
+            "query": movie_title
+        }
+        response = requests.get("https://api.themoviedb.org/3/search/movie", params=params)
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
     return render_template("add.html", form=add_form)
+
+@app.route("/find")
+def find_movie():
+    movie_id = request.args.get('id')
+    response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}", params={"api_key": os.environ.get("MOVIES_API_KEY")})
+    data = response.json()
+    print(data)
+    year = int(data["release_date"].split("-")[0])
+    new_movie = Movie(title=data["title"],
+                      img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+                      year=year,
+                      description=data["overview"])
+
+    db.session.add(new_movie)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
